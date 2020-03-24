a89c09c5dea2bed8485e347e7724aa423510d34e
khcho902 <khcho902@gmail.com>
2020-03-24 22:32:22 +0900

    start get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kycho <kycho@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/24 22:02:28 by kycho             #+#    #+#             */
+/*   Updated: 2020/03/24 22:04:30 by kycho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+
+
+int	get_next_line(int fd, char **line);
+
+
+#endif
